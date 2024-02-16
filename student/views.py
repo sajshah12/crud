af82c676b001e4e629017ba848245251bfb10f06
aqsa <syedaaqsajaved12@gmail.com>
2024-02-16 11:43:53 +0500

    modified view file

--- student/views.py
+++ student/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render,HttpResponsePermanentRedirect
 # Create your views here.
 def delete(request,id):
     if request.method == 'POST':
-        dl = user.object.get(pk,id)
+        dl = User.object.get(pk=id)
         dl.delete()
         return HttpResponsePermanentRedirect('/')
