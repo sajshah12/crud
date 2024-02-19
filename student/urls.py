86ade8b9aa04f782d43195130848bba9cd06fbde
aqsa <syedaaqsajaved12@gmail.com>
2024-02-19 13:04:41 +0500

    work of update

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,5 @@ from student import views
 urlpatterns = [
     path('',views.add,name = 'add'),
     path('delete<int:id>/',views.delete,name='delete'),
+    path('update<int:id>/', views.update, name='update'),
 ]
